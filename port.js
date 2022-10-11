0365f7914a4e6e34fcec5a08e67d86169bcee7e6
arch3030 <78534135+arch3030@users.noreply.github.com>
2022-10-11 21:05:41 +0700

    Add files via upload

---
+++ port.js
@@ -0,0 +1,49 @@
+window.onload = loadPage;
+function loadPage()
+{
+    var prof = document.getElementById("abt-btn")
+    prof.onclick = aboutmeLoadPage
+    var sh1 = document.getElementById("showmore1")
+    sh1.onclick = show1LoadPage
+    var sh2 = document.getElementById("showmore2")
+    sh2.onclick = show2LoadPage
+    var sh3 = document.getElementById("showmore3")
+    sh3.onclick = show3LoadPage
+    var ass4 = document.getElementById("as4")
+    ass4.onclick = as4LoadPage
+    var ass5 = document.getElementById("as5")
+    ass5.onclick = as5LoadPage
+    var ass6 = document.getElementById("as6")
+    ass6.onclick = as6LoadPage
+
+    
+}
+function aboutmeLoadPage()
+{
+    location.href= "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+function show1LoadPage()
+{
+    location.href= "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+function show2LoadPage()
+{
+    location.href= "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+function show3LoadPage()
+{
+    location.href= "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+function as4LoadPage()
+{
+    location.href= "https://arch3030.github.io/WEB-Assignment-4/";
+}
+function as5LoadPage()
+{
+    location.href= "https://arch3030.github.io/WEB-Asssignemnt5/";
+}
+function as6LoadPage()
+{
+    location.href= "https://arch3030.github.io/WEB-assignment6/";
+}
+
